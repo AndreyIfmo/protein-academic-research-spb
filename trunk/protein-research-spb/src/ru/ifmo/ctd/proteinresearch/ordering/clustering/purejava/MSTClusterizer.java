6bcf2a500b9d9c3a922cf20c927db775d3418547
ansokolmail@gmail.com <ansokolmail@gmail.com@13c568a5-bc96-08ee-b09a-112d78dc86a0>
2014-01-19 21:07:52 +0000

    Добавил матричный измеритель расстояния для точного кластеризатора

--- trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/ordering/clustering/purejava/MSTClusterizer.java
+++ trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/ordering/clustering/purejava/MSTClusterizer.java
@@ -2,6 +2,7 @@ package ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava;
 
 import org.biojava.bio.structure.Chain;
 import ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava.distance.Distance;
+import ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava.distance.MatrixDistance;
 import ru.ifmo.ctd.proteinresearch.ordering.graph.DisjointSets;
 import ru.ifmo.ctd.proteinresearch.ordering.graph.Graph;
 import ru.ifmo.ctd.proteinresearch.ordering.graph.MatrixGraph;
@@ -19,31 +20,44 @@ public class MSTClusterizer implements Clusterizer {
     Graph mst;
     DisjointSets sets;
     Distance distance;
+    MatrixDistance matrixDistance;
+    boolean isMatrix;
+
+    public MSTClusterizer(int numberOfClusters, MatrixDistance operator) {
+        this.numberOfClusters = numberOfClusters;
+        this.matrixDistance = operator;
+        this.isMatrix = true;
+    }
 
     public MSTClusterizer(int numberOfClusters, Distance operator) {
         this.numberOfClusters = numberOfClusters;
         this.distance = operator;
+        this.isMatrix = false;
     }
 
     @Override
     public void evaluate(List<Chain> allignedChains) {
         distanceMatrix = new double[allignedChains.size()][allignedChains.size()];
-        for (int i = 0; i< allignedChains.size(); i++) {
-            for(int j=0; j<allignedChains.size(); j++) {
-                distanceMatrix[i][j]=distance.distance(allignedChains.get(i),allignedChains.get(j));
+        for (int i = 0; i < allignedChains.size(); i++) {
+            for (int j = 0; j < allignedChains.size(); j++) {
+                if (!isMatrix) {
+                    distanceMatrix[i][j] = distance.distance(allignedChains.get(i), allignedChains.get(j));
+                } else {
+                    distanceMatrix[i][j] = matrixDistance.distance(i, j);
+                }
             }
         }
-        Graph g= new MatrixGraph(distanceMatrix.length,distanceMatrix);
-        sets=new DisjointSets(g.getN());
+        Graph g = new MatrixGraph(distanceMatrix.length, distanceMatrix);
+        sets = new DisjointSets(g.getN());
         mst = new MinimalSpanTreeFinder().getPartMST(g, numberOfClusters, sets);
     }
 
     @Override
     public List<Cluster> getClusters() {
         List<Cluster> clusters = new ArrayList<>(numberOfClusters);
-        for (int i=0; i<mst.getN(); i++) {
+        for (int i = 0; i < mst.getN(); i++) {
             int numberOfCluster = sets.find(i);
-            System.out.println(i+" "+numberOfCluster);
+            System.out.println(i + " " + numberOfCluster);
 
         }
         return null;
--- trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/ordering/clustering/purejava/PureClusterizerRunner.java
+++ trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/ordering/clustering/purejava/PureClusterizerRunner.java
@@ -1,13 +1,12 @@
 package ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava;
 
 import org.biojava.bio.structure.Chain;
+import ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava.distance.MatrixDistance;
 import ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava.distance.RMSDDistance;
-
 import ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava.distance.TMDistance;
 import ru.ifmo.ctd.proteinresearch.ordering.solvers.ConformationChain;
 import ru.ifmo.ctd.proteinresearch.ordering.solvers.ConformationGraph;
 import ru.ifmo.ctd.proteinresearch.ordering.solvers.VertexEdgeAnalyzer;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,13 +15,13 @@ import java.util.List;
  * Created by Andrey on 25.12.13.
  */
 public class PureClusterizerRunner {
-    public void run (String name) throws Exception {
-        ConformationGraph cg = new ConformationGraph(name+"_costs.txt", name+".zip", name+"/2LJI_optim%d_%d.pdb");
-        int numberOfVertices= cg.graph.getN();
+    public void run(String name) throws Exception {
+        ConformationGraph cg = new ConformationGraph(name + "_costs.txt", name + ".zip", name + "/2LJI_optim%d_%d.pdb");
+        int numberOfVertices = cg.graph.getN();
         List<Chain> chains = new ArrayList<>();
         Chain reference = VertexEdgeAnalyzer.getBasicChain(cg, 0);
         chains.add(reference);
-        for (int i=1; i<numberOfVertices; i++) {
+        for (int i = 1; i < numberOfVertices; i++) {
             chains.add(ConformationChain.align(reference, VertexEdgeAnalyzer.getBasicChain(cg, i)));
         }
         Clusterizer clusterizer = new MSTClusterizer(2, new RMSDDistance());
@@ -31,7 +30,12 @@ public class PureClusterizerRunner {
         Clusterizer clusterizer2 = new MSTClusterizer(2, new TMDistance());
         clusterizer2.evaluate(chains);
         clusterizer2.getClusters();
+
+        Clusterizer clusterizer3 = new MSTClusterizer(2, new MatrixDistance("table-2LJI.txt"));
+        clusterizer3.evaluate(chains);
+        clusterizer3.getClusters();
     }
+
     public static void main(String[] args) throws Exception {
         new PureClusterizerRunner().run("2LJI_optim");
     }
