9c25419576d51eedcf548bde6627ad387e3e85a6
ansokolmail@gmail.com <ansokolmail@gmail.com@13c568a5-bc96-08ee-b09a-112d78dc86a0>
2014-03-04 18:03:10 +0000

    Clustering small changes

--- trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/ordering/clustering/purejava/PureClusterizerRunner.java
+++ trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/ordering/clustering/purejava/PureClusterizerRunner.java
@@ -4,6 +4,8 @@ import org.biojava.bio.structure.Chain;
 import ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava.distance.MatrixDistance;
 import ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava.distance.RMSDDistance;
 import ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava.distance.TMDistance;
+import ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava.fuzzy.FuzzyClusterizer;
+import ru.ifmo.ctd.proteinresearch.ordering.graph.GraphParser;
 import ru.ifmo.ctd.proteinresearch.ordering.solvers.ConformationChain;
 import ru.ifmo.ctd.proteinresearch.ordering.solvers.ConformationGraph;
 import ru.ifmo.ctd.proteinresearch.ordering.solvers.VertexEdgeAnalyzer;
@@ -25,29 +27,28 @@ public class PureClusterizerRunner {
         for (int i = 1; i < numberOfVertices; i++) {
             chains.add(ConformationChain.align(reference, VertexEdgeAnalyzer.getBasicChain(cg, i)));
         }
-
+        MatrixDistance distanceMatrix = new MatrixDistance("2LXG/matrix.txt");
         for (int i = 2; i < 7; i++) {
-            System.out.println("============" + i + "============");
-            StructuralClusterizer clusterizer = new MSTClusterizer(i, new RMSDDistance());
-            clusterizer.evaluate(chains);
-            int counter = 1;
-            for (Cluster<Integer> it : clusterizer.getClusters()) {
-                System.out.println(counter++ + ", " + Arrays.toString(it.chainSet.toArray()));
-            }
-            StructuralClusterizer clusterizer2 = new MSTClusterizer(i, new TMDistance());
-            clusterizer2.evaluate(chains);
 
-            System.out.println("----------1------------");
-            counter = 1;
-            for (Cluster<Integer> it : clusterizer2.getClusters()) {
-                System.out.println(counter++ + ", " + Arrays.toString(it.chainSet.toArray()));
-            }
-            StructuralClusterizer clusterizer3 = new MSTClusterizer(i, new MatrixDistance("table-2LJI.txt"));
-            clusterizer3.evaluate(chains);
-            System.out.println("----------2------------");
-            counter = 1;
+            StructuralClusterizer clusterizer3 = new MSTClusterizer(i, distanceMatrix);
+            clusterizer3.evaluate(null);
+            int counter = 1;
             for (Cluster<Integer> it : clusterizer3.getClusters()) {
                 System.out.println(counter++ + ", " + Arrays.toString(it.chainSet.toArray()));
+            }
+
+        }
+        double[][] matrix = GraphParser.parseGraphMatrix("2LXG/matrix.txt");
+        for (int numOfClusters=2; numOfClusters<=3; numOfClusters++) {
+            FuzzyClusterizer fc = new FuzzyClusterizer(matrix, numOfClusters);
+            double[][] similarity = fc.evaluate();
+
+            System.out.println(Arrays.toString(fc.answerCenters));
+            for (int i = 0; i < similarity.length; i++) {
+                for (int j = 0; j < numOfClusters; j++) {
+                    System.out.print(similarity[i][j] + " ");
+                }
+                System.out.println();
             }
 
         }
