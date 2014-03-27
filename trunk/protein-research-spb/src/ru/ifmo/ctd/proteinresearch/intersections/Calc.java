08e15132c83aab6d09b956625c81c44830ffefd3
smeivan2@gmail.com <smeivan2@gmail.com@13c568a5-bc96-08ee-b09a-112d78dc86a0>
2014-03-27 13:56:21 +0000

    New Tree and Cuboid are added. Bugs are fixed.

--- trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/intersections/Calc.java
+++ trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/intersections/Calc.java
@@ -2,6 +2,5 @@ package ru.ifmo.ctd.proteinresearch.intersections;
 
 public interface Calc {
 	public int hasIntersections() throws Exception;
-	public boolean movingSegmentsIntersection(Point a1, Point b1, Point a2, Point b2, Point c1, Point c2, Point d1, Point d2) throws Exception;
-	// public void Initalize(Object ... objects);
+	public boolean movingSegmentsIntersection2Spheres(Point a1, Point b1, Point a2, Point b2, Point c1, Point c2, Point d1, Point d2) throws Exception;
 }
