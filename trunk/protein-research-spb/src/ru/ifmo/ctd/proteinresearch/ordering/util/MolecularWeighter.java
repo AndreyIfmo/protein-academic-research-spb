f77bfd9eb484e7cd28b3681b709ccd032c5c47c3
ansokolmail@gmail.com <ansokolmail@gmail.com@13c568a5-bc96-08ee-b09a-112d78dc86a0>
2014-04-07 19:25:18 +0000

    Added molecular weighter

--- trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/ordering/util/MolecularWeighter.java
+++ trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/ordering/util/MolecularWeighter.java
@@ -1,8 +1,13 @@
 package ru.ifmo.ctd.proteinresearch.ordering.util;
 
-import org.biojava.bio.structure.ChainImpl;
+import org.biojava.bio.structure.*;
+import ru.ifmo.ctd.proteinresearch.intersections.Point;
+import ru.ifmo.ctd.proteinresearch.intersections.PointsChain;
+import ru.ifmo.ctd.proteinresearch.ordering.clustering.purejava.distance.DistanceUtils;
+import ru.ifmo.ctd.proteinresearch.ordering.solvers.ConformationChain;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,8 +15,8 @@ import java.util.Map;
  */
 public class MolecularWeighter {
     private static final Map<String, Double> myMap;
-    static
-    {
+
+    static {
         myMap = new HashMap<String, Double>();
         myMap.put("ala", 71.0779);
         myMap.put("arg", 156.1857);
@@ -29,17 +34,58 @@ public class MolecularWeighter {
         myMap.put("phe", 147.1739);
         myMap.put("pro", 97.1152);
         myMap.put("ser", 87.0773);
-        myMap.put("ser", 101.1039);
+        myMap.put("thr", 101.1039);
         myMap.put("sec", 150.0379);
+        myMap.put("trp", 186.2099);
+        myMap.put("tyr", 163.1733);
+        myMap.put("val", 99.1311);
 
 
     }
-    public static double energy (ChainImpl chain1, ChainImpl chain2) {
 
-        return 0;
+    public static double energy(ConformationChain conformationChain) throws StructureException {
+        Structure structure = conformationChain.getStructure();
+        int length = structure.nrModels();
+        double w = 0;
+        for (int i = 0; i < length - 1; ++i) {
+            Chain chain1 = structure.getModel(i).get(0);
+            Chain chain2 = structure.getModel(i + 1).get(0);
+            w += energy(chain1, chain2);
+        }
+        return w;
     }
 
-    public double weight( String atomName, String residueName, boolean isFirst, boolean isLast) {
+    public static double energy(Chain chain1, Chain chain2) {
+        List<Group> atomGroups1 = chain1.getAtomGroups();
+        int size = atomGroups1.size();
+        List<Group> atomGroups2 = chain2.getAtomGroups();
+        assert size == atomGroups2.size();
+        boolean isFirst = true;
+        boolean isLast = false;
+        double totalWeight = 0;
+        for (int i = 0; i < atomGroups1.size(); i++) {
+            isLast = (i == atomGroups1.size() - 1);
+            List<Atom> atoms1 = atomGroups1.get(i).getAtoms();
+            List<Atom> atoms2 = atomGroups2.get(i).getAtoms();
+            assert atoms1.size() == 3;
+            Atom atom10 = atoms1.get(0);
+            Atom atom20 = atoms2.get(0);
+            Atom atom11 = atoms1.get(1);
+            Atom atom21 = atoms2.get(1);
+            Atom atom12 = atoms1.get(2);
+            Atom atom22 = atoms2.get(2);
+            double weight1 = weight(atom10.getName(), atomGroups1.get(i).getPDBName(), isFirst, isLast) * DistanceUtils.distance(atom10, atom20);
+            double weight2 = weight(atom11.getName(), atomGroups1.get(i).getPDBName(), isFirst, isLast) * DistanceUtils.distance(atom11, atom21);
+            double weight3 = weight(atom12.getName(), atomGroups1.get(i).getPDBName(), isFirst, isLast) * DistanceUtils.distance(atom12, atom22);
+            totalWeight += weight1;
+            totalWeight += weight2;
+            totalWeight += weight3;
+            isFirst = false;
+        }
+        return totalWeight;
+    }
+
+    public static double weight(String atomName, String residueName, boolean isFirst, boolean isLast) {
         //see http://spin.niddk.nih.gov/bax/software/TALOSORIG/backbone.gif
         switch (atomName) {
             case "C":
