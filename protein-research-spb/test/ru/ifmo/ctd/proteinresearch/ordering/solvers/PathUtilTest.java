14fb0100e9b447d51ad8a6ed1400c46825f6ae91
ansokolmail@gmail.com <ansokolmail@gmail.com@13c568a5-bc96-08ee-b09a-112d78dc86a0>
2014-04-24 07:41:04 +0000

    Restore coordinates bug fix

--- protein-research-spb/test/ru/ifmo/ctd/proteinresearch/ordering/solvers/PathUtilTest.java
+++ protein-research-spb/test/ru/ifmo/ctd/proteinresearch/ordering/solvers/PathUtilTest.java
@@ -2,6 +2,12 @@ package ru.ifmo.ctd.proteinresearch.ordering.solvers;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
+import org.biojava.bio.structure.Chain;
+import org.biojava.bio.structure.Structure;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * ansokolmail@gmail.com
@@ -12,5 +18,14 @@ public class PathUtilTest extends TestCase {
         ConformationChain cc = cg.getChain(0,1);
         int number = EvaluatedChain.getAtoms(cc.getStructure().getChain(0)).size();
         Assert.assertEquals(198, number);
+    }
+    public void testWriteChain() throws Exception {
+        ConformationGraph cg = new ConformationGraph("2LJI_optim_costs.txt", "2LJI_optim.zip", "2LJI_optim/2LJI_optim%d_%d.pdb");
+        ConformationChain cc = cg.getChain(0,9);
+        Structure structure = cc.getStructure();
+        Chain chain1 = structure.getModel(0).get(0);
+        Chain chain2 = structure.getModel(1).get(0);
+        List<Chain> chainList = PathUtil.getInterpolatedChains(chain1, chain2, 50);
+        
     }
 }
