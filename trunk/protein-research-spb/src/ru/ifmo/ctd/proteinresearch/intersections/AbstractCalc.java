aa93e68328dfe70933111b7d9a1990192856f020
smeivan2@gmail.com <smeivan2@gmail.com@13c568a5-bc96-08ee-b09a-112d78dc86a0>
2014-05-27 15:44:41 +0000

    Some bug fixes

--- trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/intersections/AbstractCalc.java
+++ trunk/protein-research-spb/src/ru/ifmo/ctd/proteinresearch/intersections/AbstractCalc.java
@@ -16,30 +16,30 @@ public abstract class AbstractCalc implements Calc {
 	}
 
 	boolean movingTooFar(Point a1, Point b1, Point a2, Point b2) {
-		double length = (b1.x - a1.x) * (b1.x - a1.x) + (b1.y - a1.y) * (b1.z - a1.z) + (b1.z - a1.z) * (b1.z - a1.z);
-		double dist1 = (a2.x - a1.x) * (a2.x - a1.x) + (a2.y - a1.y) * (a2.z - a1.z) + (a2.z - a1.z) * (a2.z - a1.z);
-		double dist2 = (b1.x - b2.x) * (b1.x - b2.x) + (b1.y - b2.y) * (b1.z - b2.z) + (b1.z - b2.z) * (b1.z - b2.z);
+		double length = (b1.x - a1.x) * (b1.x - a1.x) + (b1.y - a1.y) * (b1.y - a1.y) + (b1.z - a1.z) * (b1.z - a1.z);
+		double dist1 = (a2.x - a1.x) * (a2.x - a1.x) + (a2.y - a1.y) * (a2.y - a1.y) + (a2.z - a1.z) * (a2.z - a1.z);
+		double dist2 = (b1.x - b2.x) * (b1.x - b2.x) + (b1.y - b2.y) * (b1.y - b2.y) + (b1.z - b2.z) * (b1.z - b2.z);
 		double dist = Math.max(dist1, dist2);
 
-		return dist > 4 * length; // invariant check
+		return dist > 16 * length; // invariant check
 	}
-	
-	boolean segmentsHasIntersections(Point a1, Point b1, Point a2, Point b2, Point c1, Point c2, Point d1, Point d2) throws Exception{
-//		return movingSegmentsIntersection2Spheres(a1, b1, a2, b2, c1, c2, d1, d2);
-		
+
+	boolean segmentsHasIntersections(Point a1, Point b1, Point a2, Point b2, Point c1, Point c2, Point d1, Point d2) throws Exception {
+		return movingSegmentsIntersection2Spheres(a1, b1, a2, b2, c1, c2, d1, d2);
+
 //		double dist = distanceCalc.computeShortestDistanceForSegments(a1, b1, a2, b2, c1, c2, d1, d2, 0.1);
-//		if (dist < 0.1){
+//		if (dist < 0.1) {
 //			return true;
 //		}
-		
-		if (movingSegmentsIntersection2Spheres(a1, b1, a2, b2, c1, c2, d1, d2)){
-			double dist = distanceCalc.computeShortestDistanceForSegments(a1, b1, a2, b2, c1, c2, d1, d2, 0.1);
-			if (dist < 0.1){
-				return true;
-			}	
-		}
-//		
-		return false;
+
+//		if (movingSegmentsIntersection2Spheres(a1, b1, a2, b2, c1, c2, d1, d2)) {
+//			double dist = distanceCalc.computeShortestDistanceForSegments(a1, b1, a2, b2, c1, c2, d1, d2, 0.1);
+//			if (dist < 0.1) {
+//				return true;
+//			}
+// }
+
+//		return false;
 	}
 
 	int intersectAll(int index, Point[] first, Point[] second) throws Exception {
@@ -51,17 +51,19 @@ public abstract class AbstractCalc implements Calc {
 			if (k == index) {
 				continue;
 			}
-			
+
 			TernaryShortestDistance distanceCalc = new TernaryShortestDistance();
 
 			Point[] secondPoints = getEdgePoints(first[k - 1], first[k], second[k - 1], second[k]);
 			if (movingSegmentsAreClose(firstPoints, secondPoints) == true) {
 				// ++functionCalculations;
-//				if (movingSegmentsIntersection2Spheres(first[index - 1], first[index], second[index - 1], second[index], first[k - 1], first[k], second[k - 1], second[k]) == true) {
-//					++intersections;
-//				}
+				// if (movingSegmentsIntersection2Spheres(first[index - 1],
+				// first[index], second[index - 1], second[index], first[k - 1],
+				// first[k], second[k - 1], second[k]) == true) {
+				// ++intersections;
+				// }
 				double dist = distanceCalc.computeShortestDistanceForSegments(first[index - 1], first[index], second[index - 1], second[index], first[k - 1], first[k], second[k - 1], second[k], 0.1);
-				if (dist < 0.1){
+				if (dist < 0.1) {
 					++intersections;
 				}
 			}
@@ -151,32 +153,25 @@ public abstract class AbstractCalc implements Calc {
 		Point Ocd2 = c2.add(d2);
 		Ocd2.multHere(0.5);
 
-		double diameter = a1.distance(b1);
-		// double diameter2 = c1.distance(d1);
+		Point Ms = new Point(Oab1.x - Ocd1.x, Oab1.y - Ocd1.y, Oab1.z - Ocd1.z);
+		Point Ks = new Point(Oab2.x - Oab1.x - Ocd2.x + Ocd1.x, Oab2.y - Oab1.y - Ocd2.y + Ocd1.y, Oab2.z - Oab1.z - Ocd2.z + Ocd1.z);
 
-		// double diameter3 = a2.distance(b2);
-		// double diameter4 = c2.distance(d2);
+		Double top = Ms.x * Ks.x + Ms.y * Ks.y + Ms.z * Ks.z;
+		Double bot = Ks.x * Ks.x + Ks.y * Ks.y + Ks.z * Ks.z;
 
-		double abDist = Oab1.distance(Oab2);
-		double cdDist = Ocd1.distance(Ocd2);
-		int maxStep = (int) (Math.max(abDist, cdDist) / diameter) + 1;
+		double t = top / bot;
 
-		Point abDelta = Oab2.sub(Oab1);
-		abDelta.divideHere(maxStep);
-		Point cdDelta = Ocd2.sub(Ocd1);
-		cdDelta.divideHere(maxStep);
-
-		diameter /= 3;
-
-		for (int i = 0; i != maxStep; ++i) {
-			if (Oab1.distance(Ocd1) < diameter) {
-				return true;
-			}
-			Oab1.addHere(abDelta);
-			Ocd1.addHere(cdDelta);
+		if (t > 1) {
+			t = 1;
+		} else if (t < 0) {
+			t = 0;
 		}
 
-		if (Oab2.distance(Ocd2) < diameter) {
+		double diameter = a1.distance(b1);
+
+		double minDistance = Math.sqrt(Math.pow(Ms.x + Ks.x * t, 2) + Math.pow(Ms.y + Ks.y * t, 2) + Math.pow(Ms.z + Ks.z * t, 2));
+
+		if (minDistance < diameter) {
 			return true;
 		}
 		return false;
